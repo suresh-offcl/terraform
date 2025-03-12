158bebc472d03febe9c8c2a71bc82085bfab5117
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-03-12 11:59:46 +0530

    adding packages to install

---
+++ expense/output.tf
@@ -0,0 +1,3 @@
+output "expense_info" {
+    value = aws_instance.expense
+}
---
+++ expense/variable.tf
@@ -0,0 +1,16 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+
+    type    =  map
+    default = {
+
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+
+    }
+}
